724c69e8d607589269043f1112ad7fade2fb5ee2
Eduardo <c.edu3101@hotmail.com>
2026-05-06 15:28:51 -0300

    add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const htmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   target: "web",
@@ -9,6 +10,13 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+
+  plugins: [
+    new htmlWebPackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+      favicon: path.resolve(__dirname, "src", "assets", "scissors.svg"),
+    }),
+  ],
 
   devServer: {
     static: {
