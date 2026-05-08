f485b6b1b033be19a200b837adb2d81d1001f269
Eduardo <c.edu3101@hotmail.com>
2026-05-08 07:29:41 -0300

    finally webpack config and add lib dayjs

---
+++ src/main.js
@@ -0,0 +1,13 @@
+"use scrict";
+
+//Importação de estilização
+import "./styles/form.css";
+import "./styles/global.css";
+import "./styles/schedule.css";
+
+//Importando a lib de data e hora
+import "./libs/dayjs";
+
+import dayjs from "dayjs";
+
+console.log(dayjs().format("HH:mm"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const htmlWebPackPlugin = require("html-webpack-plugin");
+const copyWebPackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   target: "web",
@@ -16,7 +17,34 @@ module.exports = {
       template: path.resolve(__dirname, "index.html"),
       favicon: path.resolve(__dirname, "src", "assets", "scissors.svg"),
     }),
+    new copyWebPackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets"),
+          to: path.resolve(__dirname, "dist", "src", "assets"),
+        },
+      ],
+    }),
   ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 
   devServer: {
     static: {
