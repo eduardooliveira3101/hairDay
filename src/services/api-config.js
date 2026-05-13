62d043216aed8a9d9340d260694e52b5d19f4156
Eduardo <c.edu3101@hotmail.com>
2026-05-13 13:35:43 -0300

    add comments

--- src/services/api-config.js
+++ src/services/api-config.js
@@ -1,3 +1,4 @@
+//Endereço onde a API se encontra
 export const apiConfig = {
   baseURL: "http://localhost:3333",
 };
--- src/services/schedules-new.js
+++ src/services/schedules-new.js
@@ -1,17 +1,24 @@
+//Importando a o endereço da API
 import { apiConfig } from "./api-config.js";
 
+//Função que vai servir para registrar um novo agendamento dentro da api
 export async function schedulesNew({ id, name, when }) {
   try {
+    //Fazendo uma requisição para api
     await fetch(`${apiConfig}/schedules`, {
+      //Setando o método sendo 'POST' o de criação
       method: "POST",
+      //Adicionando informações ao cabeçalho
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ id, name, when }),
     });
 
+    //Gatilho para informar que o agendamento foi um sucesso
     alert("Agendamento realizado com sucesso!");
   } catch (error) {
+    //Disparando o alerta de erro
     alert("Não foi possível realizar sua requisição");
     console.log(error);
   }
